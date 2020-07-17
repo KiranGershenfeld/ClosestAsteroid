0b5571c232c52f7dc5960f9d9a9892d2620d6fb5
Kiran Gershenfeld <kiran@gershenfeld.org>
2020-07-17 13:58:20 -0700

    Initial Flask Structure

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+#Routes-------------------
+#Main page redirects to portfolio
+@app.route('/')
+def start():
+    return render_template("ClosestAsteroid.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
